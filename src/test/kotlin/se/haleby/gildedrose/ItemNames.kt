9346aae094f5efdd323793dea89ce3360da285eb
Johan Haleby <johan.haleby@gmail.com>
2022-01-28 14:16:20 +0100

    Initial commit

---
+++ src/test/kotlin/se/haleby/gildedrose/ItemNames.kt
@@ -0,0 +1,8 @@
+package se.haleby.gildedrose
+
+internal object ItemNames {
+    const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+    const val AGED_BRIE = "Aged Brie"
+    const val BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+    const val CONJURED = "Conjured Mana Cake"
+}
