96047d1bb0d6c9efd7032fd39fc1982fa12508f0
Johan Haleby <johan.haleby@gmail.com>
2022-01-31 08:22:04 +0100

    Renamed shiny to domain model

--- src/main/kotlin/se/haleby/gildedrose/GildedRose.kt
+++ src/main/kotlin/se/haleby/gildedrose/GildedRose.kt
@@ -4,13 +4,13 @@ import se.haleby.gildedrose.domainmodel.Item.*
 import se.haleby.gildedrose.domainmodel.ItemName
 import se.haleby.gildedrose.domainmodel.Quality.*
 import se.haleby.gildedrose.domainmodel.SellingRequirement.SellIn
-import se.haleby.gildedrose.domainmodel.GildedRose as NewShinyGildedRose
+import se.haleby.gildedrose.domainmodel.GildedRose as DomainModelGildedRose
 
 class GildedRose(val items: List<Item>) {
 
 
     fun updateQuality() {
-        val currentShinyItems = items.map { item ->
+        val currentDomainModelItems = items.map { item ->
             val (name, sellInDays, quality) = item
             when {
                 name == "Aged Brie" -> AgedBrie(SellIn(sellInDays), ImprovesWithAgeQuality(quality))
@@ -21,12 +21,12 @@ class GildedRose(val items: List<Item>) {
             }
         }
 
-        val newShinyItems = NewShinyGildedRose(currentShinyItems).updateQuality().items
+        val newDomainModelItems = DomainModelGildedRose(currentDomainModelItems).updateQuality().items
 
-        newShinyItems.forEachIndexed { i, newShinyItem ->
-            items[i].quality = newShinyItem.quality.value
-            if (newShinyItem.sellingRequirement is SellIn) {
-                items[i].sellIn = (newShinyItem.sellingRequirement as SellIn).numberOfDays
+        newDomainModelItems.forEachIndexed { i, newDomainModelItem ->
+            items[i].quality = newDomainModelItem.quality.value
+            if (newDomainModelItem.sellingRequirement is SellIn) {
+                items[i].sellIn = (newDomainModelItem.sellingRequirement as SellIn).numberOfDays
             }
         }
     }
